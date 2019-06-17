babad180fcd1046cac1f0ff4aa01357067ac950e
Nguyen Binh Sang <sang.nguyen@hyperlogy.com>
2019-06-17 10:17:00 +0700

    first commit

---
+++ cas-secured-app/src/main/java/com/baeldung/cassecuredapp/CasSecuredAppApplication.java
@@ -0,0 +1,92 @@
+package com.baeldung.cassecuredapp;
+
+import org.jasig.cas.client.session.SingleSignOutFilter;
+import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
+import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
+import org.jasig.cas.client.validation.Cas30ServiceTicketValidator;
+import org.jasig.cas.client.validation.TicketValidator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import javax.servlet.http.HttpSessionEvent;
+
+@SpringBootApplication
+public class CasSecuredAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasSecuredAppApplication.class, args);
+    }
+
+    @Bean
+    public ServiceProperties serviceProperties() {
+        ServiceProperties serviceProperties = new ServiceProperties();
+        serviceProperties.setService("http://sso.client.com:9797/login/cas");
+        serviceProperties.setSendRenew(false);
+        return serviceProperties;
+    }
+
+    @Bean
+    @Primary
+    public AuthenticationEntryPoint authenticationEntryPoint(ServiceProperties sP) {
+        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
+        entryPoint.setLoginUrl("https://sso.hyper.com:8443/cas/login");
+        entryPoint.setServiceProperties(sP);
+        return entryPoint;
+    }
+
+    @Bean
+    public TicketValidator ticketValidator() {
+        return new Cas20ServiceTicketValidator("https://sso.hyper.com:8443/cas");
+    }
+
+    @Bean
+    public CasAuthenticationProvider casAuthenticationProvider() {
+        CasAuthenticationProvider provider = new CasAuthenticationProvider();
+        provider.setServiceProperties(serviceProperties());
+        provider.setTicketValidator(ticketValidator());
+        provider.setUserDetailsService((gis) -> new User("test@test.com", "smatt",
+                true, true, true, true,
+                AuthorityUtils.createAuthorityList("ROLE_ADMIN")));
+        provider.setKey("CAS_PROVIDER_SSO.HYPER.COM_9797");
+        return provider;
+    }
+
+
+    @Bean
+    public SecurityContextLogoutHandler securityContextLogoutHandler() {
+        return new SecurityContextLogoutHandler();
+    }
+
+    @Bean
+    public LogoutFilter logoutFilter() {
+        LogoutFilter logoutFilter = new LogoutFilter(
+                "https://sso.hyper.com:8443/cas/logout", securityContextLogoutHandler());
+        logoutFilter.setFilterProcessesUrl("/logout/cas");
+        return logoutFilter;
+    }
+
+    @Bean
+    public SingleSignOutFilter singleSignOutFilter() {
+        SingleSignOutFilter singleSignOutFilter = new SingleSignOutFilter();
+        singleSignOutFilter.setCasServerUrlPrefix("https://sso.hyper.com:8443/cas");
+        singleSignOutFilter.setIgnoreInitConfiguration(true);
+        return singleSignOutFilter;
+    }
+
+    @EventListener
+    public SingleSignOutHttpSessionListener singleSignOutHttpSessionListener(HttpSessionEvent event) {
+        return new SingleSignOutHttpSessionListener();
+    }
+}
